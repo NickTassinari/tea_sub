47bf4c0520f6fe3ac0608c49cfdb525c3577ba8c
Nick Tassinari <ntass7800@gmail.com>
2023-09-13 11:09:17 -0600

    delete deactivated column: no need

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_12_173156) do
+ActiveRecord::Schema.define(version: 2023_09_13_170805) do
 
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 2023_09_12_173156) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "tea_id"
-    t.boolean "deactivated"
     t.index ["customer_id"], name: "index_subscriptions_on_customer_id"
     t.index ["tea_id"], name: "index_subscriptions_on_tea_id"
   end
