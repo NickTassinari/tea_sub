ffe3c93ea0dd34320d68734bd348156711c8d102
Nick Tassinari <ntass7800@gmail.com>
2023-09-14 15:05:31 -0600

    add sad path testing for cancel sub

--- spec/requests/api/v1/subscriptions/cancel_spec.rb
+++ spec/requests/api/v1/subscriptions/cancel_spec.rb
@@ -21,4 +21,25 @@ RSpec.describe "Cancel Subscriptions API" do
     
     expect(subscription_1.status).to eq("deactivated")
   end 
+
+  it "can activate a deactivated status" do 
+    customer_1 = Customer.create!(first_name: "Nick", last_name: "Tassinari", email: "nick@gmail.com", address: "504 nunya business way")
+    tea_1 = Tea.create!(title: "Sencha", description: "Japanese green tea", temperature: "100", brew_time: "3 minutes")
+    
+    subscription_1 = Subscription.create!( 
+        title: "Tea o the Month",
+        price: 19.99,
+        status: "deactivated",
+        frequency: "monthly",
+        customer_id: customer_1.id, 
+        tea_id: tea_1.id)
+    
+    patch "/api/v1/customers/#{customer_1.id}/subscriptions/#{subscription_1.id}"
+    
+    expect(response).to have_http_status(200)
+
+    subscription_1.reload
+    
+    expect(subscription_1.status).to eq("active")
+  end
 end 
