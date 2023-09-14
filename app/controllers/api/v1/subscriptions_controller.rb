00d05bf0bf642ed2b2af154cf0fd8eb84cffea72
Nick Tassinari <ntass7800@gmail.com>
2023-09-14 14:47:30 -0600

    add serializer to controller

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -5,21 +5,22 @@ module Api
       def index 
         customer = Customer.find(params[:customer_id])
         subscriptions = customer.subscriptions
-        render json: subscriptions
+        render json: SubscriptionSerializer.new(subscriptions), status: 200
       end
 
       def create 
         subscription = Subscription.new(subscription_params)
         if subscription.save 
-          render json: subscription, status: :created 
+          render json: SubscriptionSerializer.new(subscription), status: :created
         else  
           render json: subscription.errors, status: :unprocessable_entity
         end
       end
 
-      def destroy 
+      def update 
         subscription = Subscription.find(params[:id])
         subscription.update(status: "deactivated")
+        render json: SubscriptionSerializer.new(subscription)
       end
 
       private 
